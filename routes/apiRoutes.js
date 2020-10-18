5610f716f03da3bd7207ce2be93e14438b01de81
grimmgr <ggrimm33@gmail.com>
2020-10-17 18:41:48 -0700

    put route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,5 @@
 const db = require("../models");
+const mongojs = require("mongojs");
 
 module.exports = function(app) {
     app.get("/api/workouts", (req, res) => {
@@ -8,6 +9,26 @@ module.exports = function(app) {
         })
         .catch(err => {
             res.json(err);
+        })
+    });
+
+    app.post("/api/workouts", (req, res) => {
+        db.Workout.create({})
+        .then(workout => {
+            res.json(workout);
+        }).catch(err => {
+            console.log(err);
+        })
+    });
+
+    app.put("/api/workouts/:id", (req, res) => {
+        console.log(JSON.stringify(req.params.id));
+        console.log(req.body);
+        db.Workout.updateOne({ _id: mongojs.ObjectId(req.params.id) }, { $push: { exercises: req.body }})
+        .then(exercise => {
+            res.json(exercise);
+        }).catch(err => {
+            console.log(err);
         })
     });
 };
